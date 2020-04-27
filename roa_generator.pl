643d4aeff44f222d843d2a421e1420034de9d512
hehopmajieh <hehopmajieh@debian.bg>
2020-04-27 17:53:50 +0300

    Send output to file

--- roa_generator.pl
+++ roa_generator.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/perl
+# SPDX-License-Identifier: CC-BY-4.0	
+#
+#  linux/arch/arm/common/amba.c
+#
+#  Copyright (C) 2020 Mitko Gamishev Neterra Ltd, All Rights Reserved.
+#
 
 use strict;
 use warnings;
@@ -7,7 +13,6 @@ use Getopt::Long qw(GetOptions);
 use Data::Dumper qw(Dumper);
 use POSIX qw(strftime);
 
-my $date = strftime "%m/%d/%Y", localtime;
 my $host = 'whois.neterra.net';
 my @routes;
 #debug statement , disable if unneded
@@ -15,10 +20,12 @@ my $debug=0;
 my $irr = Net::IRR->connect( host => $host ) or die "Cannot connect to $host\n";
 my $roa_name;
 my $input;
+my $out_file;
 
-GetOptions('list=s' => \$input,'roa_name=s'=>\$roa_name) or die "usage: $0 [options]\n";
-if ($input && $roa_name) {
+GetOptions('list=s' => \$input,'roa_name=s'=>\$roa_name, 'out=s'=>\$out_file) or die "usage: $0 [options]\n";
+if ($input && $roa_name && $out_file) {
 	print "Check input for valid AS-SET \n" if $debug;
+	
 	if (check_if_asset($input)) { 
 		print "Valid AS-SET found \n" if $debug;
 		get_as_set($input);
@@ -32,6 +39,7 @@ if ($input && $roa_name) {
 	print "$0 Usage: \n";
 	print "--list  not defined, argument is mandatory\n";
 	print "--name  not defined, argument is mandatory\n";
+	print "--out   not defined, argument is mandatory\n";
 	print "Usage: $0 --list 'AS-SET| AS-LIST' --roa_name 'roa table name' --out 'path to save' \n";
 
 }
@@ -46,36 +54,40 @@ sub check_if_asset{
 }
 
 sub get_as_list {
+	open(FH, '>', $out_file) or die $!;
 	my @ases = @_;
-	print "#--------------Generated at :".strftime("%Y-%m-%d %H:%M:%S",localtime)."--------------\n";
-	print "roa table r$roa_name {\n";
+	print FH "#--------------Generated at :".strftime("%Y-%m-%d %H:%M:%S",localtime)."--------------\n";
+	print FH "roa table r$roa_name {\n";
 	foreach my $as (@ases) {
         push(@routes, $irr->get_routes_by_origin($as));
 
 	my @aggedroutes = aggregate(\@routes);
     foreach my $route (@aggedroutes) {
-        	print "\troa $route max 32 as $as;\n";
+        	print FH "\troa $route max 32 as $as;\n";
 		}
 	}
-	print "}\n";
-	
+	print FH "}\n";
+	close(FH);
+
 
 }
 
 
 sub get_as_set {
+	open(FH, '>', $out_file) or die $!;
 	my $asset = $_[0];
 	my @ases = $irr->get_as_set($asset, 1);
-	print "#--------------Generated at :".strftime("%Y-%m-%d %H:%M:%S",localtime)."--------------\n";
-	print "roa table r$roa_name {\n";
+	print FH "#--------------Generated at :".strftime("%Y-%m-%d %H:%M:%S",localtime)."--------------\n";
+	print FH "roa table r$roa_name {\n";
 	foreach my $as (@ases) {
     	push(@routes, $irr->get_routes_by_origin($as));
 	my @aggedroutes = aggregate(\@routes);
 	foreach my $route (@aggedroutes) {
-    	    print "\t roa $route max 32 as $as;\n";
+    	    print FH "\t roa $route max 32 as $as;\n";
 		}
 	}
-	print "}\n";
+	print FH "}\n";
+	close(FH);
 }
 
 sub aggregate {
